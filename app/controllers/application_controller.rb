dc4e0d098ff896791232eda5bc1a58171b781efc
Antonio C Neto <antoniocavalcante1910@hotmail.com>
2024-05-26 08:32:25 -0300

    feat(story/RAPI/init) testes requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::API
   def authenticate_recruiter!
     token = request.headers['Authorization']&.split(' ')&.last
     decoded_token = AuthenticationService.decode(token)
-    if decoded_token && @current_recruiter = Recruiter.find_by(id: decoded_token[:recruiter_id])
+
+    if decoded_token.is_a?(Hash) && @current_recruiter = Recruiter.find_by(id: decoded_token[:recruiter_id])
       @current_recruiter
     else
       render json: { error: 'Unauthorized' }, status: :unauthorized
--- app/services/authentication_service.rb
+++ app/services/authentication_service.rb
@@ -1,15 +1,19 @@
-class AuthenticationService
-  HMAC_SECRET = Rails.application.secrets.secret_key_base
+require 'jwt'
 
-  def self.encode(payload, exp = 24.hours.from_now)
+class AuthenticationService
+  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+
+  def self.encode(payload, exp = 10.minutes.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, HMAC_SECRET)
+    JWT.encode(payload, SECRET_KEY)
   end
 
   def self.decode(token)
-    body = JWT.decode(token, HMAC_SECRET)[0]
-    HashWithIndifferentAccess.new(body)
-  rescue
-    nil
+    decoded = JWT.decode(token, SECRET_KEY).first
+    HashWithIndifferentAccess.new(decoded)
+  rescue JWT::DecodeError => e
+    'invalid token'
+  rescue JWT::ExpiredSignature
+    'token has expired'
   end
 end
